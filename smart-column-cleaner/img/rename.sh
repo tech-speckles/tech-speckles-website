9d958c3833f7264c990e5d7136d10ab253c10524
Gagan Bansal <gaganbansal123@gmail.com>
2025-08-14 12:32:56 +0530

    feat: smart-column-cleaner product pages added

---
+++ smart-column-cleaner/img/rename.sh
@@ -0,0 +1,4 @@
+mv application_card_banner_220x140.png banner-220x240.png
+mv smart_collumn_cleaner_logo_48.png logo-48.png
+mv smart_collumn_cleaner_logo_128.png logo-128.png
+mv smart_collumn_cleaner_logo_1024.png logo-1024.png
